ff4b87dc3b648eb10d202f4a5dd51055391e76e5
Imam Hossain <imamhossain1310@gmail.com>
2020-01-04 17:13:41 +0600

    Remove unused codes from reg_visitor serializer

--- user_management/serializers/registration_visitor.py
+++ user_management/serializers/registration_visitor.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from user_management.models import User, VisitorProfile
-from django.contrib.auth.password_validation import validate_password
-from django.core.exceptions import ValidationError
 
 
 class VisitorProfileSerializer(serializers.ModelSerializer):
@@ -18,13 +16,6 @@ class VisitorRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'phone', 'password', 'password_confirmation', 'visitor_profile')
-    #
-    # def validate_password(self, value):
-    #     try:
-    #         validate_password(value)
-    #     except ValidationError as exc:
-    #         raise serializers.ValidationError(str(exc))
-    #     return value
 
     def validate(self, data):
         password = data['password']
@@ -34,13 +25,3 @@ class VisitorRegistrationSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError('Password and Password confirmation do not match')
             return data
         raise data
-    #
-    # def create(self, validated_data):
-    #     ingredients_data = validated_data.pop('visitor_profile')
-    #     recipe = User.objects.create(**validated_data)
-    #
-    #     for ingredient in ingredients_data:
-    #         ingredient, created = VisitorProfile.objects.get_or_create(name=ingredient['name'])
-    #         recipe.visitorprofile.add(ingredient)
-    #     return recipe
-
