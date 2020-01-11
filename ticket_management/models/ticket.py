f4bbb4db94de3b4bfd20eb8c469f626055f1371f
Imam Hossain <imamhossain1310@gmail.com>
2020-01-11 17:47:22 +0600

    Temp

--- ticket_management/models/ticket.py
+++ ticket_management/models/ticket.py
@@ -6,6 +6,7 @@ class Ticket(Model):
     user = models.ForeignKey('user_management.User', on_delete=models.SET_NULL, null=True)
     event = models.ForeignKey('event_management.Event', on_delete=models.CASCADE)
     serial_no = models.CharField(max_length=20)
-    seat_no = models.CharField(max_length=10)
+    seat_no = models.CharField(max_length=10, blank=True, null=True)
+    unique_id = models.TextField(max_length=20, unique=True)
     is_valid = models.BooleanField(default=True, blank=True)
     used = models.BooleanField(default=False, blank=True)
--- ticket_management/urls.py
+++ ticket_management/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'ticket_management'
 
 urlpatterns = [
-    path('<user>/get-tickets/', views.VisitorRegistrationView.as_view(), name='user_all_tickets'),
-    path('<user>/get-ticket/<event>', views.VisitorRegistrationView.as_view(), name='user_event_ticket'),
-    path('validate/', views.VisitorRegistrationView.as_view(), name='ticket_validation'),
+    # path('<user>/get-tickets/', views.VisitorRegistrationView.as_view(), name='user_all_tickets'),
+    # path('<user>/get-ticket/<event>', views.VisitorRegistrationView.as_view(), name='user_event_ticket'),
+    # path('validate/', views.VisitorRegistrationView.as_view(), name='ticket_validation'),
 ]
