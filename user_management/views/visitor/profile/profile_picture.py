b1d4020431124987387f4996d31a56ede045c1ef
Imam Hossain <imamhossain1310@gmail.com>
2020-01-21 17:15:44 +0600

    Update pro pic upload

--- user_management/views/visitor/profile/profile_picture.py
+++ user_management/views/visitor/profile/profile_picture.py
@@ -39,53 +39,31 @@ class ChangeProfilePictureView(APIView):
         data = request.data.copy()
         serializer = ChangeProfilePictureSerializer(data=data)
 
-        # print(type(request.user))
-
         if serializer.is_valid():
-            # data['user_id'] = 2
-            if 'user_id' not in data:
-                invalid_json_data.update({message_key: 'no user-id provided to change profile picture'})
-                return Response(invalid_json_data, invalid_status)
-
-            user_id = data['user_id']
-            if not type(user_id) == str or not user_id.isdigit():
-                invalid_json_data.update({message_key: 'invalid user-id type'})
-                return Response(invalid_json_data, invalid_status)
-
-            user_id = int(user_id)
-            if user_id < 1:
-                invalid_json_data.update({message_key: 'invalid user-id provided'})
-                return Response(invalid_json_data, invalid_status)
-
             profile_picture = serializer.validated_data['profile_picture']
-            # if request.user.id == url_user_id:
             if profile_picture:
-                if user_id == int(url_user_id):
-                    try:
-                        user = User.objects.get(id=user_id)
-                    except:
-                        invalid_json_data.update({message_key: 'invalid user-id provided'})
-                        return Response(invalid_json_data, invalid_status)
-                    try:
-                        VisitorProfile.objects.get(user=user)
-                    except:
-                        invalid_json_data.update({message_key: 'user is not a visitor type'})
-                        return Response(invalid_json_data, invalid_status)
+                user_id = int(url_user_id)
+                try:
+                    user = User.objects.get(id=user_id)
+                except:
+                    invalid_json_data.update({message_key: 'invalid user-id provided'})
+                    return Response(invalid_json_data, invalid_status)
+                try:
+                    VisitorProfile.objects.get(user=user)
+                except:
+                    invalid_json_data.update({message_key: 'user is not a visitor type'})
+                    return Response(invalid_json_data, invalid_status)
 
-                    user.visitorprofile.profile_picture = profile_picture
-                    user.visitorprofile.save()
+                user.visitorprofile.profile_picture = profile_picture
+                user.visitorprofile.save()
 
-                    valid_json_data = {
-                        request_key: 'valid',
-                        profile_picture_key: '{}{}'.format(settings.MEDIA_URL_HEADER,
-                                                           user.visitorprofile.profile_picture),
-                        message_key: 'profile picture updated'
-                    }
-                    return Response(valid_json_data, valid_status)
-
-                else:
-                    invalid_json_data.update({message_key: 'forbidden user: user not allowed'})
-
+                valid_json_data = {
+                    request_key: 'valid',
+                    profile_picture_key: '{}{}'.format(settings.MEDIA_URL_HEADER,
+                                                       user.visitorprofile.profile_picture),
+                    message_key: 'profile picture updated'
+                }
+                return Response(valid_json_data, valid_status)
         else:
             invalid_json_data.update({message_key: 'invalid data input'})
 
